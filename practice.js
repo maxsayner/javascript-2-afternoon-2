61726e7566a21ea90f4ad28f1e12817aea2dbfb9
Maxson Sayner <max.sayner@gmail.com>
2018-05-05 12:43:53 -0600

    done for now

--- practice.js
+++ practice.js
@@ -17,8 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -32,9 +33,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
-
+function last(arr){
+  return arr.pop();
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -47,7 +48,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family){
+for(var i = 0; i < family.length; i++){
+  alert(family[i]);
+}
+}
 
 
 
@@ -62,8 +67,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop through the given array backwards alerting every item in the array starting at the end.\
 */
 
-//Code Here
-
+function reversedLooper(letters) {
+  for(var i = letters.length - 1; i >= 0; i--) {
+alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -77,8 +85,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Return an array that contains the even numbers from the nums array.
 */
 
-//Code Here
+function evenFinder(nums){
+  var evenNumbers = [];
+  for(var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0){
+      evenNumbers.push(nums[i]);
+    }
 
+      
+  }
+return evenNumbers;
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
@@ -94,7 +111,22 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
   Have divider return an Array with the first item in the array being the evens array (all the even values from numbersArray) and the second item in the Array being the odds array (all the odd values from numbersArray).
 */
 
-//Code Here
+function divider(numbersArray){
+  var evens = [];
+  var odds = [];
+  var evensOdds = [evens,odds];
+  for(var i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] % 2 === 0) {
+      evens.push(numbersArray[i])
+    }
+     else {
+        odds.push(numbersArray[i])
+      }
+    }
+    return evensOdds;
+  }
+
+  
 
 
 
@@ -115,7 +147,6 @@ var getRandomArbitrary = function() {
   If it is, return true, if it's not, return false
 */
 
-//Code Here
 
 
 
